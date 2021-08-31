4e0534a050c4828117046f9d1de314551e954f5c
salt-n-ice <2019UEE1465@mnit.ac.in>
2021-08-31 18:19:52 +0530

    checkpoint 1

--- src/components/pre-auth-pages/front.js
+++ src/components/pre-auth-pages/front.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Button from '@material-ui/core/Button';
 import { faSchool } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {useHistory} from 'react-router-dom';
 import useStyles from './styles';
+import { UserContext } from '../../UserContext';
+import { AuthContext } from '../../AuthContext';
+import {PostContext} from '../../PostContext';
 
 export const Front = () => {
+    const { user, setUser } = useContext(UserContext);
+  const { auth, setAuth } = useContext(AuthContext);
+  const { post, setPost } = useContext(PostContext);
 
+    const history = useHistory();
+    const handleRedirectSignin = () => {
+        history.push('/signin');
+            setUser(null);
+            setAuth(false);
+            setPost({arr: []});
+    }
+    const handleRedirectSignup = () => {
+        history.push('/signup');
+        setUser(null);
+        setAuth(false);
+        setPost({arr: []});
+    }
     const classes = useStyles();
-
+    
     return (
         <div className={classes.bodyDiv}>
             <div className={classes.pageContainer}>
@@ -19,8 +39,9 @@ export const Front = () => {
                     <h1 className={classes.headingFont}> E - Classroom </h1>
                 </div>
                 <div className={classes.buttonContainer}>
-                <Button variant="contained" className={classes.buttonStyles} color="secondary" href="/signin"> Sign In </Button>
-                <Button variant="contained" className={classes.buttonStyles} color="primary" href="/signup"> Sign Up </Button>
+                <Button variant="contained" className={classes.buttonStyles} color="secondary" onClick={handleRedirectSignin}> Sign In </Button>
+                <Button variant="contained" className={classes.buttonStyles} color="primary" onClick={handleRedirectSignup}> Sign Up </Button>
+                
                 {/* <Button variant="contained" className={classes.buttonStyles} color="light" href="/dashboard"> Dashboard </Button> */}
                 </div>
                 </div>
